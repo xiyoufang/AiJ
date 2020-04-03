78e1590f339eab9d7cd78e59cbb2598d4e72e4a9
xiyoufang <xiyoufang@yeah.net>
2020-04-03 14:42:37 +0800

    初始化服务

--- aiJ-core/src/main/resources/migration/mysql/aij-platform/V20190220223350__init.sql
+++ aiJ-core/src/main/resources/migration/mysql/aij-platform/V20190220223350__init.sql
@@ -1,3 +1,7 @@
+-- 初始化服务
+INSERT INTO service (id, type, code, name, description, protected, created_time, modified_time) VALUES (1, 'PLAZA', 100002, '游戏大厅', '游戏大厅服务', 'Y', '2019-04-28 15:33:23', '2019-07-25 23:47:01');
+INSERT INTO service (id, type, code, name, description, protected, created_time, modified_time) VALUES (2, 'PLATFORM', 100001, '运营平台', '运营管理平台', 'Y', '2019-04-28 17:05:57', '2020-03-23 22:28:52');
+
 -- 初始化配置
 INSERT INTO setting (domain, code, value, description, serialized, created_time, modified_time) VALUES ('platform', 'plaza_broadcast', '["游戏仅供娱乐,严禁赌博!","游戏开发交流群:112958956"]', '大厅提示', 1, '2019-02-20 08:00:00', '2019-02-20 08:00:00');
 
